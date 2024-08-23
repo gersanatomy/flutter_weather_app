71632d7462f33f8061a2352cd90226b97cf54dc6
Germaine <dev.germaine@gmail.com>
2024-08-23 16:51:51 +0800

    chore: add initial bloc

---
+++ lib/bloc/weather/weather_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'weather_event.dart';
+part 'weather_state.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  WeatherBloc() : super(InitialWeatherState()) {}
+}
