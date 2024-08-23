0e392b672851dc13f0605fee685cfcaf81c67709
Germaine <dev.germaine@gmail.com>
2024-08-23 14:21:03 +0800

    chore: folder setup

---
+++ lib/features/weather_route.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WeatherAppRoute extends StatefulWidget {
+  const WeatherAppRoute({super.key});
+
+  @override
+  State<WeatherAppRoute> createState() => _WeatherAppRouteState();
+}
+
+class _WeatherAppRouteState extends State<WeatherAppRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(),
+    );
+  }
+}
